3ef1b8124200960d7a55926feef1a57ceecbb090
shajeebhaider <alihaidershajeeb06@gmail.com>
2025-02-21 20:53:41 +0600

    movie-app-demo

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import Search from './components/search';
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  return (
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You will Enjoy Without Any Hassle
+          </h1>
+        </header>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <h1 className="text-white">{searchTerm}</h1>
+      </div>
+    </main>
+  );
+};
+
+export default App;
